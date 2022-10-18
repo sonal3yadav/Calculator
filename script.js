1f3a295ffdb6605f050c41933b7ffa1cff965cc4
Ankit <ankit.kumar730007@gmail.com>
2022-10-19 01:23:01 +0530

    added new button

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-let string = "";
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
-    }
-    
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
-      }
-  })
-})
-  
-
-    
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+let h = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'C') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'del') {
+            screen.value = screen.value.slice(0, -1);
+            screenValue = screen.value;
+        }
+        else if (buttonText == 'sqrt') {
+            screenValue = Math.sqrt(screenValue);
+            screen.value = screenValue;
+        }
+        else if (buttonText == '=') {
+            h = screen.value + '=' + eval(screenValue);
+            screen.value = eval(screenValue);
+        }
+        else if (buttonText == 'hty') {
+            screen.value = h;
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    })
+}
